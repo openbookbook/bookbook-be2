d5f16d39ecb300f8efeb493f018b3807c7e8b694
culi <culitif@tuta.io>
2026-01-28 13:49:37 -0800

    feat: adds health check route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,23 @@
 import { Controller, Get } from '@nestjs/common';
+import { HealthCheck, HealthCheckService, TypeOrmHealthIndicator } from '@nestjs/terminus';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private health: HealthCheckService,
+    private db: TypeOrmHealthIndicator
+  ) {}
 
   @Get()
   getHello() {
     return this.appService.getHello();
+  }
+
+  @Get('health')
+  @HealthCheck()
+  check() {
+    return this.health.check([() => this.db.pingCheck('database')]);
   }
 }
